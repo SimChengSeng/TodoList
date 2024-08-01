038f553d9ddd76c75c761c07ff684cd18407618b
Sim Cheng Seng <josephsim0501@gmail.com>
2024-08-01 14:50:35 +0800

    function add,edit,delele (done)

--- todolist/src/containers/Home/Create.jsx
+++ todolist/src/containers/Home/Create.jsx
@@ -2,10 +2,10 @@ import React, { useState } from 'react'
 import './Home.css'
 import axios from 'axios'
 
-function Create({ fetchTodos }) {
-  const[task,setTask] = useState('')
-  
+function Create({fetchTodos}) {
+  const[task,setTask] = useState('');
   const handleAdd = () =>{
+
     if(!task.trim()){
       console.log('Task is empty')
       return
@@ -13,16 +13,19 @@ function Create({ fetchTodos }) {
 
     axios.post('http://localhost:3001/add',{task:task})
     .then(result => {
-      setTask('')
-      fetchTodos()
+       
+      console.log('Task after setting to empty:', task);
+      setTask('');
+      fetchTodos();
+      location.reload()
     })
+    
     .catch(err => console.log(err))
   }
 
   return (  
     <div className='create-form'>
-        <input type="text" placeholder='Enter Task' onChange={(e) => setTask(e.target.value)}
-        />
+        <input type="text" placeholder='Enter Task'onChange={(e) => setTask(e.target.value)}/>
         <button type="button" onClick={handleAdd}>Add</button>
     </div>
   )
